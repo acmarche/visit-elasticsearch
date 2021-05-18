f00e009af37366b73fdf99d01da8c4e7dbe7dfc2
jfsenechal <jfsenechal@gmail.com>
2021-05-18 21:40:16 +0200

    add search file

---
+++ search.php
@@ -0,0 +1,16 @@
+<?php
+
+use Visit\Elasticsearch\Searcher;
+
+$query = 'Ferme';
+
+$searcher = new Searcher();
+$result = $searcher->search2($query);
+echo ("Found: ".$result->count());
+echo ('-------------------');
+foreach ($result->getResults() as $result) {
+    $hit = $result->getHit();
+    $source = $hit['_source'];
+    echo $source['name'];
+}
+
