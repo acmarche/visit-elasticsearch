9da9ac401016d8b12d6a282f646787e50596778b
jfsenechal <jfsenechal@gmail.com>
2022-11-09 16:08:48 +0100

    clean comments

--- Visit/Elasticsearch/Data/DocumentElastic.php
+++ Visit/Elasticsearch/Data/DocumentElastic.php
@@ -5,36 +5,19 @@ namespace Visit\Elasticsearch\Data;
 
 class DocumentElastic
 {
-    /**
-     * @var string
-     */
     public ?string $id;
-    /**
-     * @var string
-     */
+
     public ?string $name;
-    /**
-     * @var string
-     */
+
     public ?string $excerpt;
-    /**
-     * @var string
-     */
+
     public ?string $content;
-    /**
-     * @var array
-     */
+
     public array $tags = [];
-    /**
-     * @var string
-     */
+
     public ?string $date;
-    /**
-     * @var string
-     */
+
     public ?string $url;
-    /**
-     * @var string
-     */
+
     public ?string $image;
 }
